590009bea0f96ded9c148dc4105beccc18efe10c
kajpeterson <61768919+kajpeterson@users.noreply.github.com>
2021-10-28 10:09:35 -0700

    Add files via upload

---
+++ chromedriver_test.py
@@ -0,0 +1,13 @@
+#this needs some work. Check paths
+
+from selenium import webdriver
+#import sys
+
+
+chromedriver = '/usr/local/bin/chromedriver'
+options = webdriver.ChromeOptions()
+#prefs = {"download.default_directory" : TempDir1_path}
+#options.add_experimental_option("prefs",prefs)
+#options.add_argument('headless')
+browser = webdriver.Chrome(executable_path=chromedriver, options=options) #DeprecationWarning: use options instead of chrome_options // browser = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
+browser.get('http://www.caiso.com/TodaysOutlook/Pages/supply.aspx')
